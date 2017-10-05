246e3b052a201482558e4aa63afac71d874843e3
comkar999 <31873952+comkar999@users.noreply.github.com>
2017-10-05 16:53:37 -0500

    Add files via upload

---
+++ Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Course
+    {
+        public int courseId { get; set; }
+        public string courseName { get; set; }
+       
+        //navigation properties
+
+        public List<Student> Students { get; set; }
+    }
+}
---
+++ Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Department
+    {
+
+
+          // Scalar Properties
+            public int Id { get; set; }
+            public string Name { get; set; }
+           
+
+            // Navigation Property
+            public List<Student> ListOfStudents { get; set; }
+        
+
+    }
+}
